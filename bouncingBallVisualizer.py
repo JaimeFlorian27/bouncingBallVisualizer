e5c6346ef91e8c89f9f552c4369e501d4e1ce648
jaimeforian27 <jaimeflorian27@gmail.com>
2022-05-04 00:53:29 -0700

    Start of class, added check functtion

--- bouncingBallVisualizer.py
+++ bouncingBallVisualizer.py
@@ -1,16 +1,37 @@
+from mimetypes import init
 import maya.cmds as cmds
 import maya.mel as mel
 
-#Get selected controller
-controller = cmds.ls(sl = 1)
 
-#Create sphere
+class BouncingBall:
+    def __init__(self):
+        pass
 
-sphere = cmds.polySphere(ch=1)
+    def create(self):
+    #Get selected controller
+        controller = cmds.ls(sl = 1)
 
-#Parent shape node (deleting old transform node)
+        #Create sphere
 
-sphere_shape = cmds.listRelatives(s=1, pa=1)
-cmds.parent(sphere_shape,controller, r=1, s=1)
-cmds.sl(sphere)
-mel.eval("doDelete")
+        sphere = cmds.polySphere(ch=1)
+
+        #Parent shape node (deleting old transform node)
+
+        sphere_shape = cmds.listRelatives(s=1, pa=1)
+        cmds.parent(sphere_shape,controller, r=1, s=1)
+        cmds.sl(sphere)
+
+        # doDelete checks for references and skin clusters before running delete, meaning that it wont break connections
+        mel.eval("doDelete")
+
+    def check(self):
+        '''checks if controller contains a bouncing ball '''
+        object = cmds.ls(sl=1)
+        shapes = cmds.listRelatives(object,s=1)
+        if shapes:
+            #if any of the shape nodes is a mesh
+            if any("mesh" in cmds.ls(s=1,showType=1) for s in shapes):
+                #if any of those meshes is a sphere
+                if any("Sphere" in s for s in shapes):
+                    return True
+        return False
