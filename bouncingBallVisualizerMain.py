a9d893001442a40331ce646fbab8cf90034f7dd6
jaimeforian27 <jaimeflorian27@gmail.com>
2022-05-20 14:18:19 -0700

    coding added

--- bouncingBallVisualizerMain.py
+++ bouncingBallVisualizerMain.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import os
 #sys.path.append(os.path.abspath("C:\\Users\\Usuario\\OneDrive\\Escritorio\\Arte\\Programación\\Maya\\scripts\\bouncingBallVisualizer"))
